c3a94664c008863dd74673874a32ae48b4ae2762
nachordo <ordovaspascual@gmail.com>
2020-10-24 14:52:26 +0200

    Developing an sketch of the game

--- hangman-main.py
+++ hangman-main.py
@@ -11,11 +11,32 @@ import random
 
 word = "CACAHUE"
 word_splitted = [letter for letter in word]
+letters_in_word = set(word_splitted) - set(" ")
 
 letters_chosen = []
 letters_guessed = []
+win_trigger = False
 
+letter = ""
 
-while letter == '0':
+while (letter != '0' or win_trigger):
+    letter = input("Escoge una letra: ")
+    while letter in letters_chosen:
+        letter = input("Escoge una letra diferente: ")
+    
+    #os.system("clear")
+    letters_chosen += [letter]
+    if letter in word_splitted:
+        letters_guessed += [letter]
+    
+    print(letters_guessed)
+    
+    if set(letters_guessed) == letters_in_word:
+        win_trigger = True
+    
+    
+    print(letter != '0' , not(win_trigger),letter != '0' or not(win_trigger))
+    
+        
     
 
